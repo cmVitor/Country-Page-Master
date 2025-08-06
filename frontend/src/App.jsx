bd11081ae8e96ad4e202377feb29519f7d1b9e59
Vitor Martins Castanheira <vitor.castanheira@goias.gov.br>
2025-08-06 16:17:37 -0300

    initialize project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Footer from './components/Footer'
+import './styles/App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <>
+        <div className="author-info">
+        Coded by <a href="#">Vitor M. Castanheira</a> | Challenge by
+        <a href="https://www.devchallenges.io?ref=challenge" target="_blank"
+        >devChallenges.io</a>.
+      </div>
+        </>
+    )
+}
+
+export default Footer
